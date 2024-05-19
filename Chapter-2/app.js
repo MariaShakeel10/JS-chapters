e13c65df6d18c0aec6292224f410ee0a1d7d3b6d
MariaShakeel10 <mariashakeel10a@gmail.com>
2024-05-19 14:02:41 +0500

    1st commit

---
+++ Chapter-2/app.js
@@ -0,0 +1,28 @@
+var userName;
+var myName = "Maria Shakeel";
+console.log(myName);
+
+var message;
+message = "Hello World";
+alert(message);
+
+userName = prompt("Enter your name");
+var age = prompt("Enter your age");
+var course = prompt("Which course are you enrolled in");
+alert(userName);
+alert(age);
+alert(course);
+
+var pizza ="PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(pizza);
+
+var email = "mariashakeel10a@gmail.com";
+alert("My email address is " + email);
+
+var info ="I am trying to learn by book A smarter way to learn javascript"
+alert(info)
+
+document.write ("<p>Yah! I can write Html content thourgh javascript</p>");
+
+var specialMessage ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(specialMessage)
